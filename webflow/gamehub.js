2c2e732425206a58c1c82227dd065a45d8af93b2
Ricardo Vaz <rvaz@imaginarycloud.com>
2024-01-25 15:30:57 +0000

    Imported listResponseHandler from home.js (renamed to listResponseHandlerHome)

--- webflow/gamehub.js
+++ webflow/gamehub.js
@@ -145,13 +145,90 @@ async function fetchGameLatestThreads() {
     replies: e.postcount
   }));
   const elemId = `${elemIdPrefix}-forum-threads`;
-  listResponseHandler({
+  listResponseHandlerHome({
     listData,
     elemId,
     numKeysToReplace: ["replies", "views", "upvotes"],
     textKeysToReplace: ["title", "author_name", "category_name", "topic_id", "category_id"],
   });
   $(`${elemId} .ga-loader-container`).hide();
+}
+
+function listResponseHandlerHome({
+  listData,
+  elemId,
+  numKeysToReplace,
+  textKeysToReplace,
+}) {
+  console.info(`=== ${elemId} results ===`, listData);
+  let dataTemplate = $(elemId).prop("outerHTML");
+  const $list = $(`${elemId} .gas-list`);
+  const $emptyList = $(`${elemId} .gas-list-empty`);
+  const $entryTemplate = $(".gas-list-entry", $list).first();
+  $entryTemplate.show();
+  dataTemplate = $entryTemplate.prop("outerHTML");
+  $entryTemplate.hide();
+  if (listData?.length && dataTemplate?.length) {
+    $list.html($entryTemplate);
+    listData.forEach((item, resIdx) => {
+      let dataTemplateActual = dataTemplate;
+      dataTemplateActual = dataTemplateActual.replaceAll(`{|idx|}`, resIdx + 1);
+      Object.entries(item).forEach(([key, value]) => {
+        if (item.gameIconURL?.length && !isSteamImage(item.gameIconURL)) {
+          const $gameImg = $(`.gas-list-entry-cover-game`, dataTemplateActual);
+          if ($gameImg?.length) {
+            dataTemplateActual =
+              showImageFromSrc($gameImg, item.gameIconURL) ||
+              dataTemplateActual;
+          }
+        }
+        if (
+          (item.iconURL?.length || item.imageURL?.length) &&
+          !isXboxEdsImage(item.imageURL) &&
+          !isSteamImage(item.imageURL) &&
+          !isSteamImage(item.iconURL)
+        ) {
+          const $entryImg = $(`.gas-list-entry-cover`, dataTemplateActual);
+          if ($entryImg?.length) {
+            dataTemplateActual = elemId.includes("list-games")
+              ? $entryImg
+                  .css("background-image", `url(${item.imageURL})`)
+                  .parents(".gas-list-entry")
+                  .prop("outerHTML")
+              : showImageFromSrc($entryImg, item.iconURL || item.imageURL) ||
+                dataTemplateActual;
+          }
+        }
+        if (textKeysToReplace.includes(key)) {
+          dataTemplateActual = dataTemplateActual.replaceAll(
+            `{|${key}|}`,
+            (key.endsWith("At") ? gaDate(value) : cleanupDoubleQuotes(value)) ||
+              ""
+          );
+        } else if (numKeysToReplace.includes(key)) {
+          dataTemplateActual = dataTemplateActual.replaceAll(
+            `{|${key}|}`,
+            Math.round(value || 0)
+          );
+        } else if (key === "lastPlayed") {
+          dataTemplateActual = dataTemplateActual.replaceAll(
+            `{|${key}|}`,
+            gaDate(value)
+          );
+        } else if (key === "importedFromPlatform" || key === "platform") {
+          dataTemplateActual = showPlatform(value, dataTemplateActual);
+        }
+      });
+      $list.append(dataTemplateActual);
+    });
+  } else {
+    if (listData?.length && !dataTemplate?.length) {
+      console.error(`${elemId} template issue (missing a '.gas-' class?)`);
+    }
+    $(elemId).html($emptyList);
+    $emptyList.show();
+  }
+  $list.css("display", "flex");
 }
 
 function listResponseHandler({
