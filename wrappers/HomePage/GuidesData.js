ba9e81dd38845f00811021221fe9494d8ad6ba3a
Edgar Ferreira <eferreira@imaginarycloud.com>
2024-03-27 15:46:41 +0000

    Remove pagination from home

--- wrappers/HomePage/GuidesData.js
+++ wrappers/HomePage/GuidesData.js
@@ -1,22 +1,13 @@
-import { setupPagination } from '../../utils/pagination/setupPagination';
 import { listResponseHandler } from './utils/listResponseHandler';
 
-let totalPages = 1;
-const pageBreakpoint = 7;
-
 export async function fetchGuidesData(elemId, apiDomain) {
-  const currentPage = $(`${elemId} .gas-filters-sw-li.active`).text() || 1;
-  const perPage = 4;
   const resFetch = await fetch(
-    `https://${apiDomain}/api/guide/list?perPage=${perPage}&orderBy=createdAt:desc&offset=${
-      currentPage - 1
-    }`
+    `https://${apiDomain}/api/guide/list?orderBy=createdAt:desc&perPage=4&offset=0`
   );
 
   let listData = [];
   if (resFetch.ok) {
     const resData = await resFetch.json();
-    totalPages = Math.ceil((resData?.count || 1) / perPage);
     listData = resData.results;
   }
 
@@ -32,10 +23,4 @@ export async function fetchGuidesData(elemId, apiDomain) {
 export async function loadGuides(elemIdPrefix, apiDomain) {
   const elemId = `${elemIdPrefix}-list-guides`;
   await fetchGuidesData(elemId, apiDomain);
-  setupPagination({
-    elemId: elemId,
-    fetchFn: () => fetchGuidesData(elemId, apiDomain),
-    pageBreakpoint,
-    totalPages,
-  });
 }
